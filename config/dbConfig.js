e54adb388534f31f0224c58f7e223a3958ec1dec
Simran_Yadav <simranyadav464@gmail.com>
2024-05-20 17:29:47 +0530

    Updated the URI

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.mongoURI, {
+        await mongoose.connect(process.env.MONGOURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
